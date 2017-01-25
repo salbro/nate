62d700793ab1bfe0f0fe55ab29aa886e71e7c1e1
Stephen Albro <stephenpalbro@gmail.com>
2017-01-25 17:47:18 -0500

    votesAbove and below to start working on

--- routes.py
+++ routes.py
@@ -35,6 +35,12 @@ def _vote():
     '''
     button_id_direction = request.args.get('button_id_direction', 0, type=str)
     question_id = button_id_direction.split("_")[0]
+    isTopQuestion = "top" in str(request.args.get("button_class"))
+    votes_above = request.args.get("votes_above")
+    votes_below = request.args.get("votes_below")
+    # TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO
+    print(str(votes_above))
+
 
     # vital that ids be digits only. picks 'Morality' out of 'Morality148'
     category = ''.join([i for i in str(question_id) if not i.isdigit()])
@@ -43,7 +49,10 @@ def _vote():
     with open(JSON_STORAGE, 'r') as fp:
         topic_dict = json.load(fp)
 
-    highest_vote_num = max(list(topic_dict[category].items()), key=lambda elem: elem[1][1])[1][1]
+    sorted_questions = sorted(list(topic_dict[category].items()), key=lambda elem: elem[1][1])[::-1]
+    highest_vote_num = sorted_questions[0][1][1]
+    second_highest_vote_num = sorted_questions[1][1][1]
+    # highest_vote_num = max(list(topic_dict[category].items()), key=lambda elem: elem[1][1])[1][1]
 
     if direction == "up":
         topic_dict[category][question_id][1] += 1
@@ -57,7 +66,8 @@ def _vote():
         json.dump(topic_dict, fp)
 
 
-    if topic_dict[category][question_id][1] > highest_vote_num:
+    new_num_votes = topic_dict[category][question_id][1]
+    if new_num_votes > highest_vote_num or (isTopQuestion and new_num_votes < second_highest_vote_num):
         all_questions_in_category = list(topic_dict[category].items())
         all_questions_in_category.sort(key=lambda elem: elem[1][1])
         newly_sorted_qs = all_questions_in_category[::-1]
--- static/vote.js
+++ static/vote.js
@@ -1,17 +1,23 @@
 jQuery(document).ready(function() {
-    $('.calculate').bind('click', handleClick)
+    $('.calculate').on('click', handleClick)
   });
 
     function handleClick()
 
     {
-      button_id = this.id.split("_")[0]
-      button_direction = this.id.split("_")[1]
-      button_topic = button_id.replace(/[0-9]/g, '');
+      var button_id = this.id.split("_")[0]
+      var button_direction = this.id.split("_")[1]
+      var button_topic = button_id.replace(/[0-9]/g, '');
+      var row = parseInt(this.closest('table').id.split("_")[1]);
+      var votesAbove = parseInt($("#"+button_topic+"_"+(row-1).toString()).find('span').text());
+      var votesBelow = parseInt($("#"+button_topic+"_"+(row+1).toString()).find('span').text());
       // the website can get to here.
       $.getJSON($SCRIPT_ROOT + '/_vote',
         {
-          button_id_direction: this.id
+          button_id_direction: this.id,
+          button_class: this.className,
+          votes_above: votesAbove,
+          votes_below: votesBelow
         },
           function(data) {
             /* newly_sorted_qs: questions passed back from server if a change
@@ -24,11 +30,13 @@ jQuery(document).ready(function() {
                   var q_text = question_info[1][0];
                   var q_votecount = question_info[1][1];
 
+                  if (i === 0){var top = " top";}
+                  else{var top = "";}
                   var tableHTML = "<tbody><tr><td class='question'>" + q_text + "</td> \
                     <td class='vote_cell'> \
                         <button id ='" + q_id + "_up' href='#' class='fa fa-chevron-up calculate'></button> \
                         <span id='" + q_id + "_votecount' style='color:blue;'>" + q_votecount + "</span> \
-                        <button id='" + q_id + "_down' href='#' class='fa fa-chevron-down calculate'></button> \
+                        <button id='" + q_id + "_down' href='#' class='fa fa-chevron-down calculate "+top+"'></button> \
                     </td></tr></tbody>";
                 }
 
@@ -41,7 +49,8 @@ jQuery(document).ready(function() {
               } //end for loop over questions
 
               // rebind all buttons after they've been remade in for loop
-              $('.calculate').bind('click', handleClick);
+              $('.calculate').off('click');
+              $('.calculate').on('click', handleClick);
             }
 
             else{ // no change in order necessary. just update votecount
